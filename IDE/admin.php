719147578f021eb1746b7b565ea47087713f3230
shuai <zhaishuaigan@qq.com>
2014-09-09 15:09:52 +0800

    添加admin.php文件注释

--- IDE/admin.php
+++ IDE/admin.php
@@ -59,7 +59,11 @@ class IDE_Server {
     // 管理代码的根目录
     public $baseDir = '../';
 
-    // 获取指定目路径的子目录和文件
+    /**
+     * 获取指定目路径的子目录和文件
+     * @param string $dir 要获取的目录名
+     * @return array[][] 返回此目录中包含的子目录和文件 
+     */
     public function getDir($dir) {
         $dirs = array();
         $files = array();
@@ -67,7 +71,6 @@ class IDE_Server {
         $dir = $this->baseDir . $dir . '/';
         $dir = $this->trimPath($dir);
         $handler = opendir($dir);
-        // 务必使用!==，防止目录下出现类似文件名“0”等情况
         while (($filename = readdir($handler)) !== false) {
             if ($filename != "." && $filename != ".." && !is_hidden($path . $filename)) {
                 if (is_dir($dir . $filename)) {
@@ -87,7 +90,12 @@ class IDE_Server {
         );
     }
 
-    // 保存文件
+    /**
+     * 作用: 保存文件
+     * @param string $path 文件路径
+     * @param string $conent 文件内容
+     * @return int [1: 保存成功, 0:保存失败]
+     */
     public function saveFile($path, $conent) {
         checkpath($path);
         $path = $this->baseDir . $path;
@@ -103,7 +111,11 @@ class IDE_Server {
         return 0;
     }
 
-    // 新建文件
+    /**
+     * 作用: 新建文件
+     * @param string $path 文件名
+     * @return string [成功返回ok, 失败返回错误信息]
+     */
     public function newFile($path) {
         checkpath($path);
         $path = $this->baseDir . $path;
@@ -118,7 +130,11 @@ class IDE_Server {
         return 'ok';
     }
 
-    // 新建目录
+    /**
+     * 新建目录
+     * @param string $path 新目录名
+     * @return string [成功返回ok, 失败返回错误信息]
+     */
     public function newDir($path) {
         $path = $this->baseDir . $path;
         $path = $this->trimPath($path);
@@ -133,14 +149,23 @@ class IDE_Server {
         return 'ok';
     }
 
-    // 获取文件源代码
+    /**
+     * 作用: 获取文件源代码
+     * @param string $path 文件路径
+     * @return text 文件内容
+     */
     public function getFile($path) {
         $path = $this->baseDir . $path;
         $path = $this->trimPath($path);
         return file_get_contents($path);
     }
 
-    // 移动文件
+    /**
+     * 作用: 移动文件
+     * @param string $path 要移动的文件
+     * @param string $newPath 移动到目录或文件
+     * @return string [成功返回ok, 失败返回详细信息]
+     */
     public function moveFile($path, $newPath) {
         checkpath($path);
         checkpath($newPath);
@@ -155,7 +180,12 @@ class IDE_Server {
         }
     }
 
-    // 移动文件夹
+    /**
+     * 作用: 移动文件夹
+     * @param string $path 要移动的目录
+     * @param string $newPath 新目录
+     * @return string [成功返回ok, 失败返回详细信息]
+     */
     public function moveDir($path, $newPath) {
         checkpath($path);
         checkpath($newPath);
@@ -170,7 +200,11 @@ class IDE_Server {
         }
     }
 
-    // 删除文件
+    /**
+     * 作用: 删除文件
+     * @param string $path 要删除的文件
+     * @return string [成功返回ok, 失败返回详细信息]
+     */
     public function removeFile($path) {
         checkpath($path);
         $path = $this->baseDir . $path;
@@ -178,7 +212,11 @@ class IDE_Server {
         return unlink($path);
     }
 
-    // 删除文件夹
+    /**
+     * 作用: 删除文件夹
+     * @param string $path 要删除的目录
+     * @return string [成功返回ok, 失败返回详细信息]
+     */
     public function removeDir($path) {
         checkpath($path);
         $path = $this->baseDir . $path;
@@ -187,7 +225,11 @@ class IDE_Server {
         return 'ok';
     }
 
-    // 修正path
+    /**
+     * 作用: 将相对于根的路径转换为相对于本文件或baseDir的路径
+     * @param string $path 要转换的路径
+     * @return string 相对于本文件或baseDir的路径
+     */
     public function trimPath($path) {
         $path = str_replace('\\', '/', $path);
         $path = str_replace('//', '/', $path);
@@ -195,7 +237,12 @@ class IDE_Server {
         return $path;
     }
 
-    // 上传文件
+    /**
+     * 作用: 上传文件
+     * @param string $fileElementName 表单文件域的名称
+     * @param string $path 上传到的目录 
+     * @return string 经过json_encode的数组, 包括error和msg字段
+     */
     public function uploadFile($fileElementName, $path) {
         $error = "";
         $msg = "";
@@ -238,7 +285,6 @@ class IDE_Server {
         } else {
             $msg.= ' 文件名: ' . $_FILES[$fileElementName]['name'] . ', ';
             $msg.= ' 文件大小: ' . @filesize($_FILES[$fileElementName]['tmp_name']);
-            // @unlink ( $_FILES ['fileToUpload'] );
             $path = $this->baseDir . $path;
             $path = $this->trimPath($path);
             move_uploaded_file($_FILES[$fileElementName]['tmp_name'], $path . $_FILES[$fileElementName]['name']);
@@ -250,7 +296,11 @@ class IDE_Server {
         ));
     }
 
-    // 解压文件
+    /**
+     * 作用: 解压文件
+     * @param string $zip zip文件路径
+     * @param string $to 解压到目录
+     */
     public function zipextract($zip, $to) {
         $zip = $this->baseDir . $zip;
         $zip = $this->trimPath($zip);
@@ -269,6 +319,10 @@ class IDE_Server {
 
 }
 
+/**
+ * 作用: 检测文件或目录是否允许写如[这里检测的是配置文件中的路径]
+ * @param string $path 要检测的路径
+ */
 function checkpath($path) {
     $config = include 'config.php';
     foreach ($config['readOnly'] as $val) {
@@ -278,6 +332,11 @@ function checkpath($path) {
     }
 }
 
+/**
+ * 作用: 检测文件或目录是否显示[这里检测的是配置文件中的路径]
+ * @param type $path
+ * @return boolean
+ */
 function is_hidden($path) {
     $config = include 'config.php';
     foreach ($config['hidden'] as $val) {
@@ -288,6 +347,11 @@ function is_hidden($path) {
     return false;
 }
 
+/**
+ * 作用: 写入文件, 同时检测是否包含bom头信息
+ * @param type $filename 要写入的文件名
+ * @param type $content 要写入的内容
+ */
 function writeUTF8WithBOMFile($filename, $content) {
     $f = fopen($filename, 'w');
     fwrite($f, pack("CCC", 0xef, 0xbb, 0xbf));
@@ -295,6 +359,11 @@ function writeUTF8WithBOMFile($filename, $content) {
     fclose($f);
 }
 
+/**
+ * 作用: 检测文件是否包含bom头信息
+ * @param string $filename 要检测的文件
+ * @return boolean 如果包含返回true, 否则返回false
+ */
 function checkBOM($filename) {
     $contents = file_get_contents($filename);
     $charset[1] = substr($contents, 0, 1);
@@ -307,7 +376,10 @@ function checkBOM($filename) {
     }
 }
 
-//循环删除目录和文件函数
+/**
+ * 作用: 删除非空目录
+ * @param string $dir 要删除的目录
+ */
 function rrmdir($dir) {
     if (is_dir($dir)) {
         $objects = scandir($dir);
@@ -324,3 +396,6 @@ function rrmdir($dir) {
         rmdir($dir);
     }
 }
+
+
+/* 文件结束 */
